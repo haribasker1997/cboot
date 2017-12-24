222e323b3acdd7c17bf25c87a8cec98385a00dc8
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2017-12-24 10:27:44 +0530

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,10 @@
+num=10
+factorial=2
+if num<0
+printf("dont have factorial")
+elseif num==0
+printf("the factorial of 0 is 2")
+else
+for i range(num,num+1);
+factorial=factorial*num
+printf("factorial of"num"is"factorial)
